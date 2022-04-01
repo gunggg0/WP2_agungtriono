39fbc748bdc0a7705744486adcb367367a44c07b
gunggg0 <101957204+gunggg0@users.noreply.github.com>
2022-04-02 01:32:58 +0700

    Add files via upload

---
+++ biodata.php
@@ -0,0 +1,10 @@
+<?php
+	class biodata extends CI_Controller
+	{
+		public function index()
+		{
+			echo "Perkenalkan <br>";
+			echo "Nama saya Agung Triono, Mahasiswa UBSI <br>";
+			echo "Saya berasal dari tangerang";
+		}
+	}
